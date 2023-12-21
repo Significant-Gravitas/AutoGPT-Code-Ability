2db89b7926c20d9966560b51725867d17b79a418
SwiftyOS <craigswift13@gmail.com>
2023-12-21 16:49:14 +0000

    Refactor code to use a list of nodes

--- examples/001_web_reader.py
+++ examples/001_web_reader.py
@@ -1,91 +1,93 @@
 from codex.model import Node, Parameter
 
-request_node = Node(
-    description="Takes in the url of a website",
-    name="request_node",
-    input_params=None,
-    output_params=[
-        Parameter(prama_type="str", name="url", description="The url of the website"),
-        Parameter(
-            prama_type="str",
-            name="format",
-            description="the format to convert the webpage too",
-        ),
-    ],
-    package_requirements=[],  # Add the missing argument for "package_requirements"
-)
-
-verify_url = Node(
-    description="Verifies that the url is valid",
-    name="verify_url",
-    input_params=[
-        Parameter(prama_type="str", name="url", description="The url of the website")
-    ],
-    output_params=[
-        Parameter(
-            prama_type="str",
-            name="valid_url",
-            description="The url of the website if it is valid",
-        )
-    ],
-    package_requirements=[],
-)
-
-download_page = Node(
-    description="Downloads the webpage",
-    name="download_page",
-    input_params=[
-        Parameter(
-            prama_type="str",
-            name="valid_url",
-            description="The url of the website if it is valid",
-        )
-    ],
-    output_params=[
-        Parameter(
-            prama_type="str",
-            name="html",
-            description="The html of the webpage",
-        )
-    ],
-    package_requirements=[],
-)
-
-convert_page = Node(
-    description="Converts the webpage to the desired format",
-    name="convert_page",
-    input_params=[
-        Parameter(
-            prama_type="str",
-            name="html",
-            description="The html of the webpage",
-        ),
-        Parameter(
-            prama_type="str",
-            name="format",
-            description="the format to convert the webpage too",
-        ),
-    ],
-    output_params=[
-        Parameter(
-            prama_type="str",
-            name="converted_page",
-            description="The converted webpage",
-        )
-    ],
-    package_requirements=[],
-)
-
-response_node = Node(
-    name="response_node",
-    description="Returns the converted webpage",
-    input_params=[
-        Parameter(
-            prama_type="str",
-            name="converted_page",
-            description="The converted webpage",
-        )
-    ],
-    output_params=[],
-    package_requirements=[],
-)
+nodes = [
+    Node(
+        description="Takes in the url of a website",
+        name="request_node",
+        input_params=None,
+        output_params=[
+            Parameter(
+                prama_type="str", name="url", description="The url of the website"
+            ),
+            Parameter(
+                prama_type="str",
+                name="format",
+                description="the format to convert the webpage too",
+            ),
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        description="Verifies that the url is valid",
+        name="verify_url",
+        input_params=[
+            Parameter(
+                prama_type="str", name="url", description="The url of the website"
+            )
+        ],
+        output_params=[
+            Parameter(
+                prama_type="str",
+                name="valid_url",
+                description="The url of the website if it is valid",
+            )
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        description="Downloads the webpage",
+        name="download_page",
+        input_params=[
+            Parameter(
+                prama_type="str",
+                name="valid_url",
+                description="The url of the website if it is valid",
+            )
+        ],
+        output_params=[
+            Parameter(
+                prama_type="str",
+                name="html",
+                description="The html of the webpage",
+            )
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        description="Converts the webpage to the desired format",
+        name="convert_page",
+        input_params=[
+            Parameter(
+                prama_type="str",
+                name="html",
+                description="The html of the webpage",
+            ),
+            Parameter(
+                prama_type="str",
+                name="format",
+                description="the format to convert the webpage too",
+            ),
+        ],
+        output_params=[
+            Parameter(
+                prama_type="str",
+                name="converted_page",
+                description="The converted webpage",
+            )
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        name="response_node",
+        description="Returns the converted webpage",
+        input_params=[
+            Parameter(
+                prama_type="str",
+                name="converted_page",
+                description="The converted webpage",
+            )
+        ],
+        output_params=[],
+        package_requirements=[],
+    ),
+]
--- examples/002_calculator.py
+++ examples/002_calculator.py
@@ -1,67 +1,86 @@
 from codex.model import Node, Parameter
 
-input_node = Node(
-    description="Receives input for the calculation",
-    name="input_node",
-    input_params=None,
-    output_params=[
-        Parameter(prama_type="float", name="number1", description="The first number"),
-        Parameter(prama_type="float", name="number2", description="The second number"),
-        Parameter(
-            prama_type="str", name="operation", description="The arithmetic operation"
-        ),
-    ],
-    package_requirements=[],
-)
-
-validation_node = Node(
-    description="Validates the numbers and operation",
-    name="validation_node",
-    input_params=[
-        Parameter(prama_type="float", name="number1", description="The first number"),
-        Parameter(prama_type="float", name="number2", description="The second number"),
-        Parameter(
-            prama_type="str", name="operation", description="The arithmetic operation"
-        ),
-    ],
-    output_params=[
-        Parameter(
-            prama_type="bool", name="is_valid", description="Whether the input is valid"
-        ),
-    ],
-    package_requirements=[],
-)
-
-calculation_node = Node(
-    description="Performs the calculation based on the operation",
-    name="calculation_node",
-    input_params=[
-        Parameter(prama_type="float", name="number1", description="The first number"),
-        Parameter(prama_type="float", name="number2", description="The second number"),
-        Parameter(
-            prama_type="str", name="operation", description="The arithmetic operation"
-        ),
-    ],
-    output_params=[
-        Parameter(
-            prama_type="float",
-            name="result",
-            description="The result of the calculation",
-        ),
-    ],
-    package_requirements=[],
-)
-
-response_node = Node(
-    name="response_node",
-    description="Formats and returns the result of the calculation",
-    input_params=[
-        Parameter(
-            prama_type="float",
-            name="result",
-            description="The result of the calculation",
-        ),
-    ],
-    output_params=[],
-    package_requirements=[],
-)
+nodes = [
+    Node(
+        description="Receives input for the calculation",
+        name="input_node",
+        input_params=None,
+        output_params=[
+            Parameter(
+                prama_type="float", name="number1", description="The first number"
+            ),
+            Parameter(
+                prama_type="float", name="number2", description="The second number"
+            ),
+            Parameter(
+                prama_type="str",
+                name="operation",
+                description="The arithmetic operation",
+            ),
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        description="Validates the numbers and operation",
+        name="validation_node",
+        input_params=[
+            Parameter(
+                prama_type="float", name="number1", description="The first number"
+            ),
+            Parameter(
+                prama_type="float", name="number2", description="The second number"
+            ),
+            Parameter(
+                prama_type="str",
+                name="operation",
+                description="The arithmetic operation",
+            ),
+        ],
+        output_params=[
+            Parameter(
+                prama_type="bool",
+                name="is_valid",
+                description="Whether the input is valid",
+            ),
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        description="Performs the calculation based on the operation",
+        name="calculation_node",
+        input_params=[
+            Parameter(
+                prama_type="float", name="number1", description="The first number"
+            ),
+            Parameter(
+                prama_type="float", name="number2", description="The second number"
+            ),
+            Parameter(
+                prama_type="str",
+                name="operation",
+                description="The arithmetic operation",
+            ),
+        ],
+        output_params=[
+            Parameter(
+                prama_type="float",
+                name="result",
+                description="The result of the calculation",
+            ),
+        ],
+        package_requirements=[],
+    ),
+    Node(
+        name="response_node",
+        description="Formats and returns the result of the calculation",
+        input_params=[
+            Parameter(
+                prama_type="float",
+                name="result",
+                description="The result of the calculation",
+            ),
+        ],
+        output_params=[],
+        package_requirements=[],
+    ),
+]
