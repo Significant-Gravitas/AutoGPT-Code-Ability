1a80dcf765e05ac1676915d7f77fd253ee4930bb
SwiftyOS <craigswift13@gmail.com>
2023-12-21 18:54:53 +0000

    added sample runner code

--- codex/dag.py
+++ codex/dag.py
@@ -6,6 +6,9 @@ from .model import Node
 
 
 def add_node(graph: nx.DiGraph, node_name: str, node: Node):
+    if graph.number_of_nodes() == 0:
+        graph.add_node(node_name, node=node)
+        return
     # Check if node's input parameters are satisfied by the existing nodes in the graph
     if node.input_params:
         input_params_needed = {(p.name, p.prama_type): p for p in node.input_params}
@@ -35,3 +38,27 @@ def add_node(graph: nx.DiGraph, node_name: str, node: Node):
     # Connect the new node to its parameter providers
     for param_key, provider_node in providers.items():
         graph.add_edge(provider_node, node_name, connection_type=param_key[0])
+
+
+def create_runner(graph: nx.DiGraph):
+    # Check if the graph is a DAG
+    if not nx.is_directed_acyclic_graph(graph):
+        raise nx.NetworkXError("Graph is not a Directed Acyclic Graph (DAG)")
+
+    output_name_map: Dict[str, str] = {}
+
+    script = ""
+    for node_name in nx.topological_sort(graph):
+        node: Node = graph.nodes[node_name]["node"]
+        if "request" in node_name:
+            script += node.request_to_code()
+        elif "response" in node_name:
+            script += node.response_to_code(output_name_map)
+        else:
+            code, unique_output_names_map = node.to_code(output_name_map)
+            output_name_map: Dict[str, str] = {
+                **output_name_map,
+                **unique_output_names_map,
+            }
+            script += f"{code}\n"
+    return script
--- examples/__init__.py
+++ examples/__init__.py
@@ -24,26 +24,37 @@ from examples.f_chat import (
     user_presence_nodes,
 )
 
-nodes = (
-    web_nodes
-    + calc_nodes
-    + data_nodes
-    + registration_path
-    + login_path
-    + create_post_path
-    + edit_post_path
-    + delete_post_path
-    + add_inventory_items_path
-    + delete_inventory_items_path
-    + update_inventory_items_path
-    + track_stock_levels_path
-    + registration_nodes
-    + login_nodes
-    + send_message_nodes
-    + receive_message_nodes
-    + user_presence_nodes
-    + typing_indicator_nodes
-    + read_receipts_nodes
-)
+nodes = [
+    (web_nodes, "web_nodes"),
+    (calc_nodes, "calc_nodes"),
+    (data_nodes, "data_nodes"),
+    (registration_path, "registration_path"),
+    (login_path, "login_path"),
+    (create_post_path, "create_post_path"),
+    (edit_post_path, "edit_post_path"),
+    (delete_post_path, "delete_post_path"),
+    (add_inventory_items_path, "add_inventory_items_path"),
+    (delete_inventory_items_path, "delete_inventory_items_path"),
+    (update_inventory_items_path, "update_inventory_items_path"),
+    (track_stock_levels_path, "track_stock_levels_path"),
+    (registration_nodes, "registration_nodes"),
+    (login_nodes, "login_nodes"),
+    (send_message_nodes, "send_message_nodes"),
+    (receive_message_nodes, "receive_message_nodes"),
+    (user_presence_nodes, "user_presence_nodes"),
+    (typing_indicator_nodes, "typing_indicator_nodes"),
+    (read_receipts_nodes, "read_receipts_nodes"),
+]
 
 print(f"Total number of nodes: {len(nodes)}")
+import networkx as nx
+
+from codex.dag import add_node, create_runner
+
+for graph, name in nodes:
+    print(f"Creating graph for {name}")
+    G = nx.DiGraph()
+    for node in graph:
+        add_node(G, node.name, node)
+    print(create_runner(G))
+    print("\n----------------------------------------------------\n")
