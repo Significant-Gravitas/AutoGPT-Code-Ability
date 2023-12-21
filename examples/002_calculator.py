d69285a0e8e5d0e6fbc0e09203d59d1b472b1898
SwiftyOS <craigswift13@gmail.com>
2023-12-21 15:55:11 +0000

    Add calculator example code

---
+++ examples/002_calculator.py
@@ -0,0 +1,67 @@
+from codex.model import Node, Parameter
+
+input_node = Node(
+    description="Receives input for the calculation",
+    name="input_node",
+    input_pramas=None,
+    output_pramas=[
+        Parameter(prama_type="float", name="number1", description="The first number"),
+        Parameter(prama_type="float", name="number2", description="The second number"),
+        Parameter(
+            prama_type="str", name="operation", description="The arithmetic operation"
+        ),
+    ],
+    package_requirements=[],
+)
+
+validation_node = Node(
+    description="Validates the numbers and operation",
+    name="validation_node",
+    input_pramas=[
+        Parameter(prama_type="float", name="number1", description="The first number"),
+        Parameter(prama_type="float", name="number2", description="The second number"),
+        Parameter(
+            prama_type="str", name="operation", description="The arithmetic operation"
+        ),
+    ],
+    output_pramas=[
+        Parameter(
+            prama_type="bool", name="is_valid", description="Whether the input is valid"
+        ),
+    ],
+    package_requirements=[],
+)
+
+calculation_node = Node(
+    description="Performs the calculation based on the operation",
+    name="calculation_node",
+    input_pramas=[
+        Parameter(prama_type="float", name="number1", description="The first number"),
+        Parameter(prama_type="float", name="number2", description="The second number"),
+        Parameter(
+            prama_type="str", name="operation", description="The arithmetic operation"
+        ),
+    ],
+    output_pramas=[
+        Parameter(
+            prama_type="float",
+            name="result",
+            description="The result of the calculation",
+        ),
+    ],
+    package_requirements=[],
+)
+
+response_node = Node(
+    name="response_node",
+    description="Formats and returns the result of the calculation",
+    input_pramas=[
+        Parameter(
+            prama_type="float",
+            name="result",
+            description="The result of the calculation",
+        ),
+    ],
+    output_pramas=[],
+    package_requirements=[],
+)
