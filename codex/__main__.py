4a7547f012d5ff6307d5d23a5b3ec2328da6318c
SwiftyOS <craigswift13@gmail.com>
2023-12-29 10:27:15 +0000

    Added simple server and cli tool

--- codex/__main__.py
+++ codex/__main__.py
@@ -1,1 +1,59 @@
-DATABASE_URL = "postgresql://agpt_live:bnfaHGGSDF134345@localhost:5432/agpt_product"
+import click
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+
+
+@click.command()
+@click.option(
+    "--description", prompt=True, help="Description of the code request"
+)
+@click.option("--user_id", default=1234, type=int, help="User ID")
+@click.option(
+    "--runner",
+    type=click.Choice(["Server", "CLI"], case_sensitive=False),
+    prompt=True,
+    help="Runner type",
+)
+@click.option("--username", default="admin", help="Username for authentication")
+@click.option(
+    "--password",
+    default="asd453jnsdof9384rjnsdf",
+    hide_input=True,
+    help="Password for authentication",
+)
+@click.option(
+    "--output",
+    default="downloaded_file.zip",
+    help="Output file path (default: downloaded_file.zip)",
+)
+def send_request(
+    description: str,
+    user_id: int,
+    runner: str,
+    username: str,
+    password: str,
+    output: str,
+) -> None:
+    url = "http://127.0.0.1:8000/code" 
+    data = {"description": description, "user_id": user_id, "runner": runner}
+
+    try:
+        response = requests.post(
+            url, json=data, auth=HTTPBasicAuth(username, password)
+        )
+
+        if response.status_code == 200:
+            with open(output, "wb") as f:
+                f.write(response.content)
+            click.echo(f"File downloaded successfully: {output}")
+        else:
+            click.echo(
+                f"Failed to download file. Status Code: {response.status_code}"
+            )
+    except requests.exceptions.RequestException as e:
+        click.echo(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    send_request()
---
+++ codex/app.py
@@ -0,0 +1,93 @@
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from enum import Enum
+import logging
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import zipfile
+import os
+from contextlib import asynccontextmanager
+from sqlmodel import SQLModel, create_engine, Session  # type: ignore
+
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
+
+
+# Enum for Runner
+class RunnerEnum(str, Enum):
+    server = "Server"
+    cli = "CLI"
+
+
+# Request model
+class CodeRequest(BaseModel):
+    description: str
+    user_id: int
+    runner: RunnerEnum
+
+
+# Basic authentication
+security = HTTPBasic()
+
+# Hardcoded valid credentials
+valid_credentials = {
+    "admin": "asd453jnsdof9384rjnsdf",
+    "agpt": "asdfjkl1234",
+}
+
+
+# Authentication dependency
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    if (
+        credentials.username in valid_credentials
+        and valid_credentials[credentials.username] == credentials.password
+    ):
+        return credentials.username
+    raise HTTPException(status_code=401, detail="Unauthorized")
+
+
+# FastAPI app
+app = FastAPI()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Function to create a dummy zip file
+def create_dummy_zip():
+    zip_file_path = "dummy_file.zip"
+    with zipfile.ZipFile(zip_file_path, "w") as zipf:
+        # Add a dummy file to the zip
+        dummy_file_name = "dummy.txt"
+        with open(dummy_file_name, "w") as file:
+            file.write("This is a dummy file.")
+
+        zipf.write(dummy_file_name)
+
+        # Cleanup the dummy file
+        os.remove(dummy_file_name)
+
+    return zip_file_path
+
+
+@app.post("/code")
+def handle_code_request(
+    request: CodeRequest, user: str = Depends(authenticate)
+):
+    logger.info(f"Received code request from user: {user}")
+
+    # Create a dummy zip file
+    zip_file_path = create_dummy_zip()
+
+    logger.info("Returning zip file response")
+    return FileResponse(
+        zip_file_path, media_type="application/zip", filename="dummy_file.zip"
+    )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    SQLModel.metadata.create_all(engine)
+    yield
