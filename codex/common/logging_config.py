2e4c23af1aa96455efa65f76457afcf37639cc63
SwiftyOS <craigswift13@gmail.com>
2024-01-31 17:12:07 +0100

    Add logging configuration files

---
+++ codex/common/logging_config.py
@@ -0,0 +1,12 @@
+import logging.config
+import os
+
+
+def setup_logging(local: bool = False):
+    file_path = os.path.abspath(__file__)
+    local_config = os.path.join(os.path.dirname(file_path), "log_config_local.ini")
+    cloud_config = os.path.join(os.path.dirname(file_path), "log_config_cloud.ini")
+    if local:
+        logging.config.fileConfig(local_config)
+    else:
+        logging.config.fileConfig(cloud_config)
