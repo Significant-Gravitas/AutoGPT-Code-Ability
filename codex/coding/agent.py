4a85c9a8824deacf67dae71413bd1fd6418c4037
SwiftyOS <craigswift13@gmail.com>
2024-02-02 09:48:52 +0100

    Add code for writing code graphs and coding functions

--- codex/coding/agent.py
+++ codex/coding/agent.py
@@ -1,12 +1,44 @@
 import logging
+from typing import List
+
+from codex.chains.code_graph import CodeGraph, FunctionDef
+from codex.chains.write_node import write_code_chain
 
 logger = logging.getLogger(__name__)
 
 
-def create_code():
+def write_code_graphs(code_graphs: List[CodeGraph]):
+    completed_graphs = []
+    for code_graph in code_graphs:
+        completed_cg = code_functions(code_graph)
+        completed_graphs.append(completed_cg)
+    return completed_graphs
+
+
+def code_functions(code_graph: CodeGraph) -> CodeGraph:
+    """
+    Code the functions in the code graph
+    """
+    functions = []
+    for function_name, function_def in code_graph.function_defs.items():
+        logger.info(f"Coding function {function_name}")
+        function = create_code(code_graph.name, function_def)
+        functions.append(function)
+    code_graph.functions = functions
+    return code_graph
+
+
+def create_code(appilcation_context: str, function_def: FunctionDef) -> str:
     """
     Gets the coding agent to write or lookup the code for a function
-
-    TODO: Work out the interface for this
     """
-    pass
+    # TODO: Add code lookup
+    function = write_code_chain(
+        invoke_params={
+            "node": function_def,
+            "application_context": appilcation_context,
+            "function_template": function_def.template,
+        }
+    )
+    # TODO: Add function storage
+    return function
