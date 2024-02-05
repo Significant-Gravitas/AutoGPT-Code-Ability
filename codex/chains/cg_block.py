80b7af7be6c6d33c4286d3e190b195d487f934e0
SwiftyOS <craigswift13@gmail.com>
2024-02-05 16:24:53 +0100

    Added saving of llm calls

--- codex/chains/cg_block.py
+++ codex/chains/cg_block.py
@@ -35,6 +35,7 @@ class PromptTemplateInvocationError(Exception):
 class ValidatedResponse:
     response: Any
     usage_statistics: CompletionUsage
+    message: str
 
     def __init__(self, response: Any, usage_statistics: CompletionUsage):
         self.response = response
@@ -57,7 +58,9 @@ class Validator:
             raise NotImplementedError("Function calls are not supported")
 
         return ValidatedResponse(
-            response=message.content, usage_statistics=usage_statistics
+            response=message.content,
+            usage_statistics=usage_statistics,
+            message=message.content,
         )
 
     def validate(self, invoke_params: dict, response: ValidatedResponse) -> Any:
@@ -104,59 +107,98 @@ class AIBlock:
         self.storeage_object = storeage_object
         self.is_json_response = is_json_response
         self.oai_client = oai_client
-        self.template_base_path = templates_dir = os.path.join(
-                os.path.dirname(__file__), f"../{template_base_path}/{self.model}"
-            )
-        self.template_dir = pathlib.Path(self.templates_dir).resolve(strict=True)
+        self.template_base_path = os.path.join(
+            os.path.dirname(__file__), f"../{template_base_path}/{self.model}"
+        )
+        self.templates_dir = pathlib.Path(self.template_base_path).resolve(strict=True)
+        
         self.generate_template_hash()
-        self.store_call_template()
+        self.call_template_id = None
 
     def generate_template_hash(self):
         template_str = ""
-        with open(f"{self.templates_dir}/{self.prompt_template_name}.system.j2", "r") as f:
+        with open(
+            f"{self.templates_dir}/{self.prompt_template_name}.system.j2", "r"
+        ) as f:
             template_str += f.read()
-        
-        with open(f"{self.templates_dir}/{self.prompt_template_name}.user.j2", "r") as f:
+
+        with open(
+            f"{self.templates_dir}/{self.prompt_template_name}.user.j2", "r"
+        ) as f:
             template_str += f.read()
-            
-        with open(f"{self.templates_dir}/{self.prompt_template_name}.retry.j2", "r") as f:
+
+        with open(
+            f"{self.templates_dir}/{self.prompt_template_name}.retry.j2", "r"
+        ) as f:
             template_str += f.read()
-            
+
         self.template_hash = hashlib.md5(template_str.encode()).hexdigest()
-        
-    
-    def store_call_template(self):
+
+    async def store_call_template(self):
         from prisma.models import LLMCallTemplate
+
         db = Prisma(auto_register=True)
-        await db.connect() 
-        
+        await db.connect()
+
         call_template = await LLMCallTemplate.prisma().create(
             data={
                 "templateName": self.prompt_template_name,
                 "fileHash": self.template_hash,
-                }
+            }
         )
-        
+
         await db.disconnect()
         self.call_template_id = call_template.id
         return call_template
-                
+
+    async def store_call_attempt(
+        self, response: ValidatedResponse, attempt: int, prompt: str
+    ):
+        from prisma.models import LLMCallAttempt
+
+        db = Prisma(auto_register=True)
+
+        call_attempt = await LLMCallAttempt.prisma().create(
+            data={
+                "id": self.call_template_id,
+                "completionTokens": response.usage_statistics.completion_tokens,
+                "promptTokens": response.usage_statistics.prompt_tokens,
+                "totalTokens": response.usage_statistics.total_tokens,
+                "attempt": attempt,
+                "prompt": prompt,
+                "completion": response.message,
+            }
+        )
+
+        await db.connect()
+        return call_attempt
 
     def load_temaplate(self, template: str, invoke_params: dict) -> str:
         try:
             templates_env = Environment(loader=FileSystemLoader(self.templates_dir))
-            prompt_template = templates_env.get_template(f"{self.prompt_template_name}.{template}.j2")
+            prompt_template = templates_env.get_template(
+                f"{self.prompt_template_name}.{template}.j2"
+            )
             return prompt_template.render(**invoke_params)
         except Exception as e:
             logger.error(f"Error loading template: {e}")
             raise PromptTemplateInvocationError(f"Error loading template: {e}")
 
+    @staticmethod
+    def messages_to_prompt_string(messages: list) -> str:
+        prompt = ""
+        for message in messages:
+            prompt += f"### {message['role']}\n\n {message['content']}\n\n"
+        return prompt
+
     async def invoke(self, invoke_params: dict, max_retries=3) -> Any:
+        
+        if not self.call_template_id:
+            await self.store_call_template()
+            
         retries = 0
         try:
-            system_prompt = self.load_temaplate(
-                "system", invoke_params
-            )
+            system_prompt = self.load_temaplate("system", invoke_params)
             user_prompt = self.load_temaplate("user", invoke_params)
 
             request_params = {
@@ -172,15 +214,20 @@ class AIBlock:
                 request_params["response_format"] = {"type": "json_object"}
 
             response = self.oai_client.chat.completions.create(**request_params)
+
+            await self.store_call_attempt(
+                response,
+                retries,
+                self.messages_to_prompt_string(request_params["messages"]),
+            )
+
             validated_response = self.validator.run(invoke_params, response)
         except ValidationError as e:
             while retries < max_retries:
                 retries += 1
                 try:
                     invoke_params["error_msg"] = str(e)
-                    retry_prompt = self.load_temaplate(
-                        "retry", invoke_params
-                    )
+                    retry_prompt = self.load_temaplate("retry", invoke_params)
                     request_params["messages"] = (
                         [
                             {"role": "system", "content": system_prompt},
@@ -188,6 +235,11 @@ class AIBlock:
                         ],
                     )
                     response = self.oai_client.chat.completions.create(**request_params)
+                    await self.store_call_attempt(
+                        response,
+                        retries,
+                        self.messages_to_prompt_string(request_params["messages"]),
+                    )
                     validated_response = self.validator.run(invoke_params, response)
                     break
                 except Exception as e:
@@ -198,33 +250,36 @@ class AIBlock:
         except Exception as e:
             logger.error(f"Error invoking AIBlock: {e}")
             raise LLMFailure(f"Error invoking AIBlock: {e}")
-        
+
         await self.save_output(validated_response)
 
         return validated_response.response
 
     async def save_output(self, validated_response: ValidatedResponse):
         from prisma.models import CodeGraph
+
         db = Prisma(auto_register=True)
-        await db.connect() 
-        
+        await db.connect()
+
         cg = await CodeGraph.prisma().create(
             data={
                 "function_name": validated_response.response.function_name,
                 "api_route": validated_response.response.api_route,
                 "code_graph": validated_response.response.code_graph,
-                "imports": validated_response.response.imports,            }
+                "imports": validated_response.response.imports,
+            }
         )
-        
+
         await db.disconnect()
-        
+
         return cg
-        
+
     async def update_item(self, item: Any):
         from prisma.models import CodeGraph
+
         db = Prisma(auto_register=True)
         await db.connect()
-        
+
         cg = await CodeGraph.prisma().update(
             where={"id": item.id},
             data={
@@ -234,42 +289,47 @@ class AIBlock:
                 "imports": item.imports,
             },
         )
-        
+
         await db.disconnect()
-        
+
         return cg
-    
+
     async def get_item(self, item_id: str):
         from prisma.models import CodeGraph
+
         db = Prisma(auto_register=True)
         await db.connect()
-        
+
         cg = await CodeGraph.prisma().find_unique(where={"id": item_id})
-        
+
         await db.disconnect()
-        
+
         return cg
-    
+
     async def delete_item(self, item_id: str):
         from prisma.models import CodeGraph
+
         db = Prisma(auto_register=True)
         await db.connect()
-        
+
         cg = await CodeGraph.prisma().delete(where={"id": item_id})
-        
+
         await db.disconnect()
-        
+
     async def list_items(self, item_id: str, page: int, page_size: int):
         from prisma.models import CodeGraph
+
         db = Prisma(auto_register=True)
         await db.connect()
-        
-        cg = await CodeGraph.prisma().find_many(skip=(page - 1) * page_size, take=page_size)
-        
+
+        cg = await CodeGraph.prisma().find_many(
+            skip=(page - 1) * page_size, take=page_size
+        )
+
         await db.disconnect()
-        
+
         return cg
-        
+
     def routes(self):
         base_router = APIRouter()
 
@@ -317,11 +377,9 @@ if __name__ == "__main__":
 
     ois_client = OpenAI()
 
-    block = AIBlock(
+    block =  AIBlock(
         name="code-graph",
-        system_prompt_template="cg.python.system",
-        user_prompt_template="cg.python.user",
-        retry_prompt_template="cg.python.retry",
+        prompt_template_name="cg.python",
         model="gpt-4-0125-preview",
         validator=Validator(),
         is_json_response=False,
@@ -329,11 +387,12 @@ if __name__ == "__main__":
         oai_client=ois_client,
         db_client=None,
     )
-    ans = asyncio.run(block.invoke(
-        {
-            "api_route": "/api/v1/availability",
-            "function_name": "check_availability",
-            "description": """### **Overview**
+    ans = asyncio.run(
+        block.invoke(
+            {
+                "api_route": "/api/v1/availability",
+                "function_name": "check_availability",
+                "description": """### **Overview**
 
 The function is designed to return the real-time availability status of professionals, dynamically updated based on their current activity or schedule. It operates without the need for database access, relying instead on real-time or pre-set schedule data provided at the time of the query.
 
@@ -354,8 +413,10 @@ The function is designed to return the real-time availability status of professi
 1. **Availability Status:** A response indicating the professional's current availability status. The status can be:
     - 'Available' - The professional is free and can accept appointments.
     - 'Busy' - The professional is currently occupied and cannot accept appointments.""",
-        }
-    ))
+            }
+        )
+    )
 
     import IPython
+
     IPython.embed()
