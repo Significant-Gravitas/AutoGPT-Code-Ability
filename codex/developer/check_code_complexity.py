8446548b1d09fe2da47c121a7150fbd9ad678eaa
SwiftyOS <craigswift13@gmail.com>
2024-02-06 12:55:43 +0100

    Added code complexity ai block

---
+++ codex/developer/check_code_complexity.py
@@ -0,0 +1,27 @@
+import json
+
+from codex.common.ai_block import AIBlock, ValidatedResponse, ValidationError
+from codex.developer.model import CheckComplexity
+
+
+class CheckComplexityAIBlock(AIBlock):
+    prompt_template_name = "check_code_complexity"
+    model = "gpt-4-0125-preview"
+    is_json_response = True
+
+    def validate(
+        self, invoke_params: dict, response: ValidatedResponse
+    ) -> ValidatedResponse:
+        json_data = json.loads(response.response)
+
+        try:
+            model = CheckComplexity(**json_data)
+            response.response = model
+        except Exception as e:
+            raise ValidationError(f"Error validating response: {e}")
+
+        return response
+
+    async def create_item(self, validated_response: ValidatedResponse):
+        """This is just a temporary that doesnt have a database model"""
+        pass
