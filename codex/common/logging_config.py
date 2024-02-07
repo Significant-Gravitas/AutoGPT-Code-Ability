724b17db6e4c1671a8d8ed5e32a282333a32fbaf
SwiftyOS <craigswift13@gmail.com>
2024-02-07 10:47:07 +0100

    updated logging

--- codex/common/logging_config.py
+++ codex/common/logging_config.py
@@ -1,13 +1,29 @@
 import logging.config
 import os
-
+import coloredlogs
 
 def setup_logging(local: bool = False):
     file_path = os.path.abspath(__file__)
-    local_config = os.path.join(os.path.dirname(file_path), "log_config_local.ini")
     cloud_config = os.path.join(os.path.dirname(file_path), "log_config_cloud.ini")
     if local:
-        logging.config.fileConfig(local_config)
+        log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+        # Set up basic configuration with standard output
+        logging.basicConfig(level=logging.INFO, format=log_format)
+        coloredlogs.install(level="INFO", fmt=log_format)
+
+        # Create a file handler for error messages
+        file_handler = logging.FileHandler("codex_logs.log")
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter(log_format))
+
+        # Create a file handler for error messages
+        err_file_handler = logging.FileHandler("error_logs.log")
+        err_file_handler.setLevel(logging.ERROR)
+        err_file_handler.setFormatter(logging.Formatter(log_format))
+
+        # Add the file handler to the root logger
+        logging.getLogger().addHandler(file_handler)
+        logging.getLogger().addHandler(err_file_handler)
     else:
         logging.config.fileConfig(cloud_config)
 
