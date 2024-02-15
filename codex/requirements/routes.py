c6ee5fb2ebf00a8fdab6943b5f99431763794b8e
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-02-15 10:55:22 -0600

    fix: missed await

--- codex/requirements/routes.py
+++ codex/requirements/routes.py
@@ -33,7 +33,7 @@ async def create_spec(user_id: int, app_id: int, description: str):
         app = await codex.database.get_app_by_id(user_id, app_id)
         ids = Indentifiers(user_id=user_id, app_id=app_id)
 
-        spec = generate_requirements(ids, app.name, description)
+        spec = await generate_requirements(ids, app.name, description)
         return SpecificationResponse.from_specification(spec)
     except Exception as e:
         return Response(
