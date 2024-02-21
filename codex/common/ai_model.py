927e275619064c88d7e04187ff0db5cc633053cf
Swiftyos <Swiftyos@users.noreply.github.com>
2024-02-21 09:49:13 +0000

    style fixes by ruff

--- codex/common/ai_model.py
+++ codex/common/ai_model.py
@@ -25,19 +25,21 @@ class OpenAIChatClient:
         self.openai = AsyncOpenAI(**openai_config)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     import asyncio
+
     OpenAIChatClient.configure(openai_config={})
     client = OpenAIChatClient.get_instance()
     request_params = {
         "model": "gpt-3.5-turbo",
         "messges": [
-                        {"role": "system", "content": "Answer dead good"},
-                        {"role": "user", "content": "What is the capital of USA"},
-                    ]
+            {"role": "system", "content": "Answer dead good"},
+            {"role": "user", "content": "What is the capital of USA"},
+        ],
     }
-    response = asyncio.run(client.openai.chat.completions.create(
-        model=request_params["model"],
-        messages=request_params["messges"]
-    ))
+    response = asyncio.run(
+        client.openai.chat.completions.create(
+            model=request_params["model"], messages=request_params["messges"]
+        )
+    )
     print(response)
