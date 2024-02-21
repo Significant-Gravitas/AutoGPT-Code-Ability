591279cbe79a83127b0e9642ffc12e08b9ac4fb8
SwiftyOS <craigswift13@gmail.com>
2024-02-21 11:46:43 +0100

    Add custom logger that prints exception stack trace on error

--- codex/common/logging_config.py
+++ codex/common/logging_config.py
@@ -1,7 +1,23 @@
+import logging
 import logging.config
 import os
 
 import coloredlogs
+
+
+# This is a custom logger that prints the exception stack trace on error by default
+class Logger(logging.Logger):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def error(self, msg, *args, **kwargs):
+        if not kwargs.get("exc_info"):
+            kwargs["exc_info"] = True
+        super().error(msg, *args, **kwargs)
+
+
+#  def getLogger(name):
+#      return setup_logging(Logger(name))
 
 
 def setup_logging(local: bool = False):
