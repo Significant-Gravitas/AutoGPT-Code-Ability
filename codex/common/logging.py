75b3815e3a195808bac4585980412168e7b6557e
SwiftyOS <craigswift13@gmail.com>
2024-02-21 11:48:08 +0100

    added back logging

---
+++ codex/common/logging.py
@@ -0,0 +1,18 @@
+import logging
+
+from codex.common.logging_config import setup_logging
+
+
+# This is a custom logger that prints the exception stack trace on error by default
+class Logger(logging.Logger):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def error(self, msg, *args, **kwargs):
+        if not kwargs.get("exc_info"):
+            kwargs["exc_info"] = True
+        super().error(msg, *args, **kwargs)
+
+
+# def getLogger(name):
+#     return setup_logging(Logger(name))
