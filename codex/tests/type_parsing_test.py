cefe6ce9e9ad045dead5b4223aadba5d13cce572
majdyz <zamil@majdy.local>
2024-03-07 22:51:26 +0800

    [AGPT-489] Add auto-generation of Typing Import

--- codex/tests/type_parsing_test.py
+++ codex/tests/type_parsing_test.py
@@ -6,8 +6,11 @@ from codex.common.model import extract_field_type, is_type_equal
 @pytest.mark.unit
 def test_extract_field_type():
     assert extract_field_type("str") == {"str"}
-    assert extract_field_type("tuple[str, dict[str, int]]") == {"str", "int"}
+    assert extract_field_type("tuple[str, dict[str, int]]") == {"str", "int", "Tuple", "Dict"}
     assert extract_field_type("Tuple[Obj1, Dict[Obj2, Obj3 | Obj4], Obj1]") == {
+        "Union",
+        "Tuple",
+        "Dict",
         "Obj1",
         "Obj2",
         "Obj3",
