774d125803e00af91c994ceb11a0e8f5aced3aa2
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-03-11 22:35:10 -0500

    fix: benchmark needs to use the port

--- codex/benchmark.py
+++ codex/benchmark.py
@@ -22,6 +22,7 @@ from codex.requirements.model import ExampleTask
 
 logger = logging.getLogger(__name__)
 
+port = os.environ.get("PORT", 8000)
 
 async def develop_application(
     session: aiohttp.ClientSession, task: ExampleTask, user_id: str, app_id: str
@@ -30,7 +31,7 @@ async def develop_application(
     spec = await get_latest_specification(user_id, app_id)
     spec_id = spec.id
     logger.info(f"[{task.value}] Developing the application")
-    url = f"http://127.0.0.1:8000/api/v1/user/{user_id}/apps/{app_id}/specs/{spec_id}/deliverables/"
+    url = f"http://127.0.0.1:{port}/api/v1/user/{user_id}/apps/{app_id}/specs/{spec_id}/deliverables/"
     async with session.post(url, timeout=1200) as response:
         try:
             creation_time = datetime.now()
@@ -38,7 +39,7 @@ async def develop_application(
                 return {"app_name": spec.name, "status": "failed"}
             data = await response.json()
             deliverable_id = data["id"]
-            deploy_url = f"http://127.0.0.1:8000/api/v1/user/{user_id}/apps/{app_id}/specs/{spec_id}/deliverables/{deliverable_id}/deployments/"
+            deploy_url = f"http://127.0.0.1:{port}/api/v1/user/{user_id}/apps/{app_id}/specs/{spec_id}/deliverables/{deliverable_id}/deployments/"
             async with session.post(deploy_url) as dresponse:
                 deploy_data = await dresponse.json()
                 deployment_id = deploy_data["id"]
@@ -46,7 +47,7 @@ async def develop_application(
 
                 # Download the zip file
                 download_url = (
-                    f"http://127.0.0.1:8000/api/v1/deployments/{deployment_id}/download"
+                    f"http://127.0.0.1:{port}/api/v1/deployments/{deployment_id}/download"
                 )
                 async with session.get(download_url) as download_response:
                     content = await download_response.read()
@@ -118,7 +119,7 @@ async def run_benchmark_example(
 ):
     try:
         codex_client = CodexClient(
-            client=client, base_url="http://127.0.0.1:8000/api/v1"
+            client=client, base_url=f"http://127.0.0.1:{port}/api/v1"
         )
         await codex_client.init(codex_user_id=user_id)
         app_id = ExampleTask.get_app_id(task=task)
@@ -201,3 +202,4 @@ async def run_benchmark(skip_requirements: bool, task: ExampleTask | None = None
                     )
 
     await client.disconnect()
+3
