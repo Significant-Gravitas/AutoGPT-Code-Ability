d008326c2b1093760151c3e7c9dbd6227984f17c
majdyz <zamil@majdy.local>
2024-03-12 16:12:32 +0700

    [AGPT-485] Fix missing pydantic object during requirement-step; leading to LLM to guess wrong signature

--- codex/common/model.py
+++ codex/common/model.py
@@ -223,6 +223,13 @@ async def create_object_type(
 
     field_inputs = []
     for field in fields:
+        for related_types in field.related_types or []:
+            if related_types.name in available_objects:
+                continue
+            available_objects = await create_object_type(
+                related_types, available_objects
+            )
+
         field_inputs.append(
             {
                 "name": field.name,
