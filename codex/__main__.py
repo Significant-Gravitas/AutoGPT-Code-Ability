ca14b55eeb78e16eabcb7508fb90acf462963263
SwiftyOS <craigswift13@gmail.com>
2024-03-14 17:03:24 +0100

    Refactor populate_db function and add get_resume_points command

--- codex/__main__.py
+++ codex/__main__.py
@@ -6,7 +6,6 @@ import click
 from dotenv import load_dotenv
 
 from codex.common.logging_config import setup_logging
-from codex.interview.agent import populate_database_interviews
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +28,6 @@ def populate_db(database):
     from prisma import Prisma
 
     from codex.database import create_test_data
-    from codex.requirements.agent import populate_database_specs
 
     os.environ["DATABASE_URL"] = os.environ["DATABASE_URL"] or database
     db = Prisma(auto_register=True)
@@ -37,8 +35,8 @@ def populate_db(database):
     async def popdb():
         await db.connect()
         await create_test_data()
-        await populate_database_interviews()
-        await populate_database_specs()
+        # await populate_database_interviews()
+        # await populate_database_specs()
         await db.disconnect()
 
     asyncio.run(popdb())
@@ -85,6 +83,119 @@ def example(hardcoded: bool):
 
     task = examples[case - 1]
     asyncio.run(run_benchmark(hardcoded, task))
+
+
+async def get_resume_points():
+    from prisma import Prisma
+    from prisma.models import ResumePoint
+
+    prisma_client = Prisma(auto_register=True)
+
+    await prisma_client.connect()
+
+    resume_points = await ResumePoint.prisma().find_many()
+
+    print(
+        f"{'':<3} | {'updatedAt':<20} | {'name':<30} | {'Interview':<10} | {'Specification':<15} | {'CompletedApp':<12} | {'Deployment':<10}"
+    )
+    separation_row = f"{'-' * 3}-+-{'-' * 20}-+-{'-' * 30}-+-{'-' * 10}-+-{'-' * 15}-+-{'-' * 12}-+-{'-' * 10}"
+    print(separation_row)
+    # Print table rows
+    for i, resume_point in enumerate(resume_points):
+        row = [
+            str(i + 1),
+            resume_point.updatedAt.isoformat().split(".")[0],
+            resume_point.name,
+            "✓" if resume_point.interviewId else "X",
+            "✓" if resume_point.specificationId else "X",
+            "✓" if resume_point.completedAppId else "X",
+            "✓" if resume_point.deploymentId else "X",
+        ]
+        print(
+            f"{row[0]:<3} | {row[1]:<20} | {row[2]:<30} | {row[3]:<10} | {row[4]:<15} | {row[5]:<12} | {row[6]:<10}"
+        )
+    return resume_points
+
+
+@cli.command()
+def resume():
+    import codex.benchmark
+
+    print("")
+    resume_points = asyncio.run(get_resume_points())
+    print("\n")
+    case = int(input("Select index of the app you want to resume: "))
+
+    selected_application = resume_points[case - 1]
+
+    resume_prompt = "You can resume from, "
+    resume_options = []
+    valid_options = set()
+
+    if selected_application.interviewId:
+        resume_options.append("Interview (i)")
+        valid_options.add("i")
+        valid_options.add("I")
+    if selected_application.specificationId:
+        resume_options.append("Specification (s)")
+        valid_options.add("s")
+        valid_options.add("S")
+    if selected_application.completedAppId:
+        resume_options.append("Completed App (c)")
+        valid_options.add("c")
+        valid_options.add("C")
+    if selected_application.deploymentId:
+        resume_options.append("Deployment (d)")
+        valid_options.add("d")
+        valid_options.add("D")
+
+    resume_prompt += ", ".join(resume_options) + "."
+    resume_prompt += " Enter the letter of the option you want to resume from: "
+
+    selection = input(resume_prompt)
+    if selection not in valid_options:
+        print(
+            f"Invalid selection: {selection}. Please select from the options provided."
+        )
+        return
+
+    if selection in set(["i", "I"]):
+        print("Resuming from interview...")
+        asyncio.run(
+            codex.benchmark.resume_from_interview(
+                selected_application.applicationId,
+                selected_application.interviewId,
+                selected_application,
+            )
+        )
+
+    if selection in set(["s", "S"]):
+        print("Resuming from specification...")
+        asyncio.run(
+            codex.benchmark.resume_from_specification(
+                selected_application.applicationId,
+                selected_application.specificationId,
+                selected_application,
+            )
+        )
+
+    if selection in set(["c", "C"]):
+        print("Resuming from completed app...")
+        asyncio.run(
+            codex.benchmark.resume_from_completed_app(
+                selected_application.applicationId,
+                selected_application.completedAppId,
+                selected_application,
+            )
+        )
+
+    if selection in set(["d", "D"]):
+        print("Resuming from deployment...")
+        asyncio.run(
+            codex.benchmark.resume_from_deployment(
+                selected_application.applicationId, selected_application.deploymentId
+            )
+        )
 
 
 @cli.command()
--- codex/benchmark.py
+++ codex/benchmark.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 import aiohttp
 import click
+from prisma.models import Deployment, ResumePoint
 
 import codex.common.test_const as test_const
 from codex.api_model import (
@@ -26,7 +27,11 @@ port = os.environ.get("PORT", 8000)
 
 
 async def develop_application(
-    session: aiohttp.ClientSession, task: ExampleTask, user_id: str, app_id: str
+    session: aiohttp.ClientSession,
+    task: ExampleTask,
+    user_id: str,
+    app_id: str,
+    resume_point: ResumePoint,
 ) -> dict[str, str]:
     start_time = datetime.now()
     spec = await get_latest_specification(user_id, app_id)
@@ -38,13 +43,25 @@ async def develop_application(
             creation_time = datetime.now()
             if response.status != 200:
                 return {"app_name": spec.name, "status": "failed"}
+
             data = await response.json()
+
+            await ResumePoint.prisma().update(
+                where={"id": resume_point.id},
+                data={"CompletedApp": {"connect": {"id": data["id"]}}},
+            )
+
             deliverable_id = data["id"]
             deploy_url = f"http://127.0.0.1:{port}/api/v1/user/{user_id}/apps/{app_id}/specs/{spec_id}/deliverables/{deliverable_id}/deployments/"
             async with session.post(deploy_url) as dresponse:
                 deploy_data = await dresponse.json()
                 deployment_id = deploy_data["id"]
                 deployment_file_name = deploy_data["file_name"]
+
+                await ResumePoint.prisma().update(
+                    where={"id": resume_point.id},
+                    data={"Deployment": {"connect": {"id": deployment_id}}},
+                )
 
                 # Download the zip file
                 download_url = f"http://127.0.0.1:{port}/api/v1/deployments/{deployment_id}/download"
@@ -73,13 +90,22 @@ async def develop_application(
             return {"app_name": spec.name, "status": "failed"}
 
 
-async def create_requirements(codex_client: CodexClient, task: ExampleTask):
+async def create_requirements(
+    codex_client: CodexClient, task: ExampleTask, resume_point: ResumePoint
+):
     creation_time = datetime.now()
 
     start_interview = await codex_client.start_interview(
         name=task.value, task=ExampleTask.get_task_description(task)
     )
     next_interview: InterviewResponse = start_interview
+    updated_resume_point = await ResumePoint.prisma().update(
+        where={"id": resume_point.id},
+        data={"Interview": {"connect": {"id": start_interview.id}}},
+    )
+
+    if updated_resume_point:
+        resume_point = updated_resume_point
 
     while not next_interview.finished:
         logger.info(
@@ -110,6 +136,12 @@ async def create_requirements(codex_client: CodexClient, task: ExampleTask):
     logger.info(
         f"[{task.value}] Specification created in {spec_finish_time - interview_finish_time}"
     )
+
+    updated_resume_point = await ResumePoint.prisma().update(
+        where={"id": resume_point.id},
+        data={"Specification": {"connect": {"id": spec.id}}},
+    )
+
     return spec
 
 
@@ -117,6 +149,7 @@ async def run_benchmark_example(
     session, client, task, user_id, skip_requirements: bool
 ):
     try:
+        resume_point = None
         codex_client = CodexClient(
             client=client, base_url=f"http://127.0.0.1:{port}/api/v1"
         )
@@ -126,15 +159,32 @@ async def run_benchmark_example(
         if not skip_requirements:
             logger.info(f"[{task.value}] Creating requirements")
             app = await codex_client.create_app(app_name=task.value)
+            resume_point = await ResumePoint.prisma().create(
+                data={
+                    "Application": {"connect": {"id": app_id}},
+                    "User": {"connect": {"id": user_id}},
+                    "name": task.value,
+                }
+            )
             app_id = app.id
-            await create_requirements(codex_client, task)
+            await create_requirements(codex_client, task, resume_point)
 
+        if not resume_point:
+            resume_point = await ResumePoint.prisma().create(
+                data={
+                    "Application": {"connect": {"id": app_id}},
+                    "User": {"connect": {"id": user_id}},
+                    "name": task.value,
+                }
+            )
         if not app_id:
             raise AssertionError(
                 f"[{task.value}] App ID not found for task: {task.value}"
             )
 
-        results = await develop_application(session, task, user_id, app_id)
+        results = await develop_application(
+            session, task, user_id, app_id, resume_point
+        )
         return results
     except Exception as e:
         logger.exception(f"[{task.value}] Error running benchmark: {e}")
@@ -207,3 +257,122 @@ async def run_benchmark(skip_requirements: bool, task: ExampleTask | None = None
                     )
 
     await client.disconnect()
+
+
+async def resume_from_interview(
+    app_id: str, interview_id: str, resume_point: ResumePoint
+):
+    if not app_id and not interview_id:
+        raise ValueError(
+            "You must create an app and participate in an interview before generating a spec"
+        )
+
+    url = f"http://127.0.0.1:{port}/user/{resume_point.userId}/apps/{app_id}/specs/"
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url,
+                timeout=3000,
+                params={"interview_id": interview_id},
+            ) as response:
+                response.raise_for_status()
+                spec_response = SpecificationResponse(**await response.json())
+                await ResumePoint.prisma().update(
+                    where={"id": resume_point.id},
+                    data={"Specification": {"connect": {"id": spec_response.id}}},
+                )
+                await resume_from_specification(app_id, spec_response.id, resume_point)
+
+    except aiohttp.ClientError as e:
+        logger.exception(f"Error generating app spec: {e}")
+        raise e
+    except Exception as e:
+        logger.exception(f"Unknown Error when trying to generate the spec: {e}")
+        raise e
+
+
+async def resume_from_specification(
+    app_id: str, spec_id: str, resume_point: ResumePoint
+):
+    async with aiohttp.ClientSession() as session:
+        url = f"http://127.0.0.1:{port}/api/v1/user/{resume_point.userId}/apps/{app_id}/specs/{spec_id}/deliverables/"
+        async with session.post(url, timeout=1200) as response:
+            if response.status != 200:
+                raise AssertionError(f"Failed to create deliverable: {response.status}")
+
+            data = await response.json()
+
+            await ResumePoint.prisma().update(
+                where={"id": resume_point.id},
+                data={"CompletedApp": {"connect": {"id": data["id"]}}},
+            )
+            await resume_from_completed_app(app_id, data["id"], resume_point)
+
+
+async def resume_from_completed_app(
+    app_id: str, completed_app_id: str, resume_point: ResumePoint
+):
+    async with aiohttp.ClientSession() as session:
+        try:
+            user_id = resume_point.userId
+            spec_id = resume_point.specificationId
+            if not spec_id:
+                spec = await get_latest_specification(user_id, app_id)
+                spec_id = spec.id
+            deliverable_id = completed_app_id
+            deploy_url = f"http://127.0.0.1:{port}/api/v1/user/{user_id}/apps/{app_id}/specs/{spec_id}/deliverables/{deliverable_id}/deployments/"
+            async with session.post(deploy_url) as dresponse:
+                deploy_data = await dresponse.json()
+                deployment_id = deploy_data["id"]
+                deployment_file_name = deploy_data["file_name"]
+
+                await ResumePoint.prisma().update(
+                    where={"id": resume_point.id},
+                    data={"Deployment": {"connect": {"id": deployment_id}}},
+                )
+
+                # Download the zip file
+                download_url = f"http://127.0.0.1:{port}/api/v1/deployments/{deployment_id}/download"
+                async with session.get(download_url) as download_response:
+                    content = await download_response.read()
+                    content = io.BytesIO(content)
+
+                # Unzip the file
+                extracted_folder = f"workspace/{deployment_file_name.split('.')[0]}"
+                if os.path.exists(extracted_folder):
+                    shutil.rmtree(extracted_folder)
+
+                # Create a new directory
+                if not os.path.exists(extracted_folder):
+                    os.makedirs(extracted_folder)
+                with zipfile.ZipFile(content, "r") as zip_ref:
+                    zip_ref.extractall(extracted_folder)
+        except Exception as e:
+            logger.exception(f"Error fetching deliverable: {e}")
+
+
+async def resume_from_deployment(app_id: str, deployment_id: str):
+    async with aiohttp.ClientSession() as session:
+        deployment = await Deployment.prisma().find_first_or_raise(
+            where={"id": deployment_id}
+        )
+        deployment_file_name = deployment.fileName
+        # Download the zip file
+        download_url = (
+            f"http://127.0.0.1:{port}/api/v1/deployments/{deployment_id}/download"
+        )
+        async with session.get(download_url) as download_response:
+            content = await download_response.read()
+            content = io.BytesIO(content)
+
+        # Unzip the file
+        extracted_folder = f"workspace/{deployment_file_name.split('.')[0]}"
+        if os.path.exists(extracted_folder):
+            shutil.rmtree(extracted_folder)
+
+        # Create a new directory
+        if not os.path.exists(extracted_folder):
+            os.makedirs(extracted_folder)
+        with zipfile.ZipFile(content, "r") as zip_ref:
+            zip_ref.extractall(extracted_folder)
