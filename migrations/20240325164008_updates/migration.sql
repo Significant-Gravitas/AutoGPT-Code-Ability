a6d6d37a11d9b1a4f02dbc761a1d5e7d88d50984
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-03-25 11:41:18 -0500

    fix: add a migration that wasn't included in the init :'(

---
+++ migrations/20240325164008_updates/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `model` to the `LLMCallTemplate` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "LLMCallTemplate" ADD COLUMN     "model" TEXT NOT NULL;
