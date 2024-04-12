8bbb5329eb558f04abc868835400522e73baea4e
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-04-12 17:45:31 -0500

    fix: missed await

--- codex/develop/code_validation.py
+++ codex/develop/code_validation.py
@@ -482,7 +482,7 @@ async def __execute_pyright(
 
             # Grab any enhancements we can for the error
             error_message: str = f"{e['message']}. {e.get('rule', '')}"
-            ids = get_ids_from_function_id_and_compiled_route(
+            ids = await get_ids_from_function_id_and_compiled_route(
                 func.function_id, compiled_route_id=func.compiled_route_id
             )
             if error_enhancements := await get_error_enhancements(
