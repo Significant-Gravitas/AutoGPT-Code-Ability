aadb80e30335e0ccc8035eedb735f97059a93162
SwiftyOS <craigswift13@gmail.com>
2024-04-15 12:38:09 +0200

    Updating the requirements flow

---
+++ codex/requirements/agent_v2.py
@@ -0,0 +1,83 @@
+import logging
+
+from prisma.models import Application, Specification
+
+import codex.interview.database
+import codex.requirements.blocks.ai_database
+import codex.requirements.blocks.ai_module_v2
+import codex.requirements.model
+from codex.api_model import Identifiers
+
+logger = logging.getLogger(__name__)
+
+
+async def generate_requirements(ids: Identifiers, app: Application) -> Specification:
+    """
+    Runs the Requirements Agent to generate the system requirements based
+    upon the provided task
+
+    The new V2 version of the requirements agent uses a simplifed flow:
+
+    1. Get the last interview step
+    2. Generate the modules
+    3. Generate the database design
+    4. [Parrallelized] For each module, generate the API Routes it needs
+    5. [Parrallelized] For each API Route, generate the detailed API Spec
+
+    In future there will be a simple and advanced mode for generating the requirements
+
+    The simple mode will only allow the user to interact at the interview level
+    The advanced mode will allow the user to interact at the module and API level
+
+    Args:
+        ids (Identifiers): Relevant ids for database operations
+        description (str): description of the application
+
+    Returns:
+        ApplicationRequirements: The system requirements for the application
+    """
+    if not ids.user_id or not ids.app_id:
+        raise ValueError("User and App Ids are required")
+
+    logger.info("State Object Created")
+
+    # User Interview
+    interview = await codex.interview.database.get_last_interview_step(
+        interview_id=ids.interview_id, app_id=ids.app_id
+    )
+
+    features_string = ""
+    for feature in interview.features:
+        features_string += "\nFeature Name: " + str(feature.name)
+        features_string += "\nFunctionality: " + str(feature.functionality)
+        features_string += "\nReasoning: " + str(feature.reasoning)
+        features_string += "\n"
+
+    module_block = codex.requirements.blocks.ai_module_v2.ModuleGenerationBlock()
+    module_response = await module_block.invoke(
+        ids=ids,
+        invoke_params={
+            "poduct_name": app.name,
+            "product_description": app.description,
+            "features": interview.features,
+        },
+    )
+
+    logger.info("DB Started")
+    # Database Design
+    database_block = codex.requirements.blocks.ai_database.DatabaseGenerationBlock()
+
+    product_spec_str = f"{app.name} - {app.description}\nFeatures:\n{features_string}"
+
+    db_invoke_params = {
+        "product_spec": product_spec_str,
+        "needed_auth_roles": ", ".join(a for a in module_response.access_level),
+        "modules": ", ".join(module.name for module in module_response.modules),
+    }
+
+    db_response: codex.requirements.model.DBResponse = await database_block.invoke(
+        ids=ids,
+        invoke_params=db_invoke_params,
+    )
+
+    logger.info(f"DB Finished {db_response}")
