c2ad115f58f4bc4f9da0452650a7a3b4d123aa6b
Nicholas Tindle <nicktindle@outlook.com>
2024-04-15 08:36:42 -0500

    fix(db): handle migration appropriately (#211)

--- migrations/20240410175822_access_level_protected_public/migration.sql
+++ migrations/20240410175822_access_level_protected_public/migration.sql
@@ -7,7 +7,12 @@
 -- AlterEnum
 BEGIN;
 CREATE TYPE "AccessLevel_new" AS ENUM ('PUBLIC', 'PROTECTED');
-ALTER TABLE "APIRouteSpec" ALTER COLUMN "AccessLevel" TYPE "AccessLevel_new" USING ("AccessLevel"::text::"AccessLevel_new");
+ALTER TABLE "APIRouteSpec" ALTER COLUMN "AccessLevel" TYPE "AccessLevel_new" USING (
+  CASE "AccessLevel"
+    WHEN 'PUBLIC' THEN 'PUBLIC'::text::"AccessLevel_new"
+    ELSE 'PROTECTED'::text::"AccessLevel_new"
+  END
+);
 ALTER TYPE "AccessLevel" RENAME TO "AccessLevel_old";
 ALTER TYPE "AccessLevel_new" RENAME TO "AccessLevel";
 DROP TYPE "AccessLevel_old";
