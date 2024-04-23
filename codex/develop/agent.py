dfa0f3a820954e9173928f7cedc81bb92769c373
Nicholas Tindle <nicktindle@outlook.com>
2024-04-23 07:21:57 -0500

    Raise Enhancements up to the level of the prompts (#226)
    * feat: raise the validations up
    
    * feat: add error enhancements to the AI Block
    
    * fix: extract the TODO_COMMENT to the constants
    
    * fix: move ErrorEnhancements
    
    * fix: move TODO_COMMENT
    
    * feat: add interview ID to the log
    
    * feat: enhancement is used in system prompt
    
    * fix: imports in the gen_test
    
    * feat: add decimal support
    
    * fix(develop): don't error when the only issue is llm errors

--- codex/develop/agent.py
+++ codex/develop/agent.py
@@ -17,6 +17,7 @@ import codex
 import codex.common
 import codex.common.database
 from codex.api_model import Identifiers
+from codex.common.ai_block import LLMFailure
 from codex.common.database import INCLUDE_FUNC
 from codex.common.model import FunctionDef
 from codex.develop.compile import (
@@ -134,7 +135,21 @@ async def develop_application(ids: Identifiers, spec: Specification) -> Complete
             tasks.append(task)
 
         # Run the tasks concurrently
-        await asyncio.gather(*tasks)
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        if any(isinstance(r, Exception) for r in results):
+            exceptions = [r for r in results if isinstance(r, Exception)]
+            # if the only exceptions are LLMFailures, we can continue
+            if all(isinstance(r, LLMFailure) for r in exceptions):
+                logger.warning(
+                    f"App Id: {ids.app_id} Eating Errors developing API routes: {exceptions}"
+                )
+                pass
+            else:
+                raise ValueError(
+                    f"App Id: {ids.app_id} Error developing API routes: {exceptions}"
+                )
+        else:
+            logger.info("🚀 All API routes developed successfully")
 
     return completed_app
 
