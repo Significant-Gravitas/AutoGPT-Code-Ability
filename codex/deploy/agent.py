d32fbef405fa87a0acd94331d29b389997abb645
Aarushi <50577581+aarushik93@users.noreply.github.com>
2024-04-25 16:37:32 +0100

    temporary fix for unique constraints (#251)
    * temporary fix for unique constraints
    
    * fix formatting
    
    * use user id, app name and deliverable id
    
    * fix name
    
    * linting fix
    
    * Apply suggestions from code review
    
    Co-authored-by: codiumai-pr-agent-pro[bot] <151058649+codiumai-pr-agent-pro[bot]@users.noreply.github.com>
    
    * fix formatting
    
    ---------
    
    Co-authored-by: codiumai-pr-agent-pro[bot] <151058649+codiumai-pr-agent-pro[bot]@users.noreply.github.com>

--- codex/deploy/agent.py
+++ codex/deploy/agent.py
@@ -1,7 +1,6 @@
 import base64
 import logging
 import os
-import uuid
 
 from prisma.models import CompletedApp, Deployment, Specification
 from prisma.types import DeploymentCreateInput
@@ -36,10 +35,14 @@ async def create_local_deployment(
 
     zip_file = await create_zip_file(app, spec)
     file_name = completedApp.name.replace(" ", "_")
-
+    trunc_user_id = ids.user_id[-6:]
+    trunc_deliverable_id = ids.completed_app_id[-6:]
+    unique_prefix = f"{trunc_user_id}_{file_name}_{trunc_deliverable_id}"
     try:
         base64.b64encode(zip_file)
-        logger.info(f"Creating deployment for {completedApp.name}")
+        logger.info(
+            f"Creating deployment for {completedApp.name} with repo ID {unique_prefix}"
+        )
         encoded_file_bytes = base64.b64encode(zip_file).decode("utf-8")
         deployment = await Deployment.prisma().create(
             data=DeploymentCreateInput(
@@ -49,11 +52,9 @@ async def create_local_deployment(
                 fileSize=len(zip_file),
                 # I need to do this as the Base64 type in prisma is not working
                 fileBytes=encoded_file_bytes,  # type: ignore
-                dbName="",
-                dbUser="",
-                repo=str(
-                    uuid.uuid4()
-                ),  # repo has unique constraint so we need to generate a random string
+                dbName=f"{unique_prefix}_db",
+                dbUser=f"{unique_prefix}_user",
+                repo=f"{unique_prefix}_repo",
             )
         )
     except Exception as e:
