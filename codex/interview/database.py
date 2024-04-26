c6396aeb07270a528da2b9c719d68ff3c261348b
Nicholas Tindle <nicholas.tindle@agpt.co>
2024-04-25 21:42:08 -0500

    ref: extract prisma blocks for reuse in gen_test.py

--- codex/interview/database.py
+++ codex/interview/database.py
@@ -1,6 +1,47 @@
 import prisma
 from prisma.models import Interview, InterviewStep
 
+from codex.api_model import Identifiers
+from codex.interview.model import UnderstandRequest
+
+
+async def create_interview(ids: Identifiers, name: str, description: str) -> Interview:
+    if not ids.user_id:
+        raise AssertionError("User ID not found")
+    if not ids.app_id:
+        raise AssertionError("Application ID not found")
+    interview = await prisma.models.Interview.prisma().create(
+        data={
+            "User": {"connect": {"id": ids.user_id}},
+            "Application": {"connect": {"id": ids.app_id}},
+            "name": name,
+            "task": description,
+        }
+    )
+    return interview
+
+
+async def create_interview_steps(ids: Identifiers, ans: UnderstandRequest):
+    await prisma.models.InterviewStep.prisma().create(
+        data=prisma.types.InterviewStepCreateInput(
+            Interview={"connect": {"id": ids.interview_id}},
+            Application={"connect": {"id": ids.app_id}},
+            phase_complete=ans.phase_completed,
+            say=ans.say_to_user,
+            thoughts=ans.thoughts,
+            Features=prisma.types.FeatureCreateManyNestedWithoutRelationsInput(
+                create=[
+                    prisma.types.FeatureCreateWithoutRelationsInput(
+                        name=f.name,
+                        reasoning=f.reasoning,
+                        functionality=f.functionality,
+                    )
+                    for f in ans.features or []
+                ]
+            ),
+        )
+    )
+
 
 async def delete_interview(interview_id: str) -> None:
     await Interview.prisma().update(
