22ad62c63b37452a78882c6f13b35f6d7cc60daa
Aarushi <aarushik93@gmail.com>
2024-04-26 18:22:52 +0100

    remove unused setttings

--- codex/settings/database.py
+++
@@ -1,9 +0,0 @@
-from prisma.models import Settings
-
-
-async def get_settings(user_id: str) -> Settings:
-    settings = await Settings.prisma().find_unique_or_raise(
-        where={"user_id": user_id, "deleted": False},
-    )
-
-    return settings
